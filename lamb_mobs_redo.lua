62f9504e58fb592c86bb24d220ebf10c243b09dc
runs <runs@runs-PC.runs.es>
2019-03-16 12:32:15 +0100

    lambs!

--- lamb_mobs_redo.lua
+++ lamb_mobs_redo.lua
@@ -64,8 +64,7 @@ mobs:register_mob("petz:"..pet_name, {
 	jump = true,
 	follow = petz.settings.lamb_follow,
 	drops = {
-		{name = "mobs:meat_raw", chance = 1, min = 1, max = 1,},
-		{name = "petz:lamb_fur", chance = 1, min = 1, max = 1,}
+		{name = "mobs:meat_raw", chance = 1, min = 1, max = 1,},		
 	},
 	water_damage = 0,
 	lava_damage = 6,
