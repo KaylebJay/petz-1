cd33a3a89ab85e499769a70b48c172f102b5cc85
runs <runs@runs-PC.runs.es>
2019-08-26 17:55:27 +0200

    fire damage

--- petz/api/api_env_damage.lua
+++ petz/api/api_env_damage.lua
@@ -4,7 +4,7 @@ petz.env_damage = function(self)
 	local stand_pos= mobkit.get_stand_pos(self)
 	local stand_node_pos = mobkit.get_node_pos(stand_pos)
 	local stand_node = mobkit.nodeatpos(stand_node_pos)
-	if stand_node.groups.igniter then --if lava or fire
+	if stand_node and stand_node.groups.igniter then --if lava or fire
 		mobkit.hurt(self, petz.settings.igniter_damage)
 	end
 end
