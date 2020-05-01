294b5c91a2dd5aefa52363f4ad5ab6bf8976fa5f
runs <runs@runs-PC.runs.es>
2020-05-01 17:41:06 +0200

    fixes

--- petz/misc/items.lua
+++ petz/misc/items.lua
@@ -269,7 +269,8 @@ minetest.register_craftitem("petz:glass_syringe", {
 minetest.register_craftitem("petz:glass_syringe_sperm", {
     description = S("Glass Syringe with seed"),
     inventory_image = "petz_glass_syringe_sperm.png",
-    wield_image = "petz_glass_syringe_sperm.png"
+    wield_image = "petz_glass_syringe_sperm.png",
+    stack_max = 1
 })
 
 minetest.register_craft({
